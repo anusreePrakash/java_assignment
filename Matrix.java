c202eab02d79f05d2282c2d2d6de29343bc135e0
Anusree <anusree@thoughtworks.com>
2016-02-19 12:51:34 +0530

    Added method for multiplying two mertices.

--- Matrix.java
+++ Matrix.java
@@ -1,11 +1,12 @@
 public class Matrix {
     private int row, column, value;
     private int[][] matrix;
+    private int sum = 0;
     public Matrix(int row, int column){
         this.row = row;
         this.column = column;
         this.matrix = new int[row][column];
-    }
+    };
 
     public void populateMatrix(int row, int column, int value){
         this.matrix[row][column] = value;
@@ -15,14 +16,49 @@ public class Matrix {
         return this.matrix[row][column];
     };
 
+    public boolean isSameOrder(Matrix other){
+        if(this.row == other.row && this.column == other.column)
+        return  true;
+        return false;
+    }
+
+    public boolean isEqualMatrix(Matrix other){
+        if(this.matrix == other.matrix)
+        return true;
+        return false;
+    }
+
     public Matrix addMatrix(Matrix other){
-        Matrix result = new Matrix(this.row, this.column);
-        for (int i = 0; i<this.row ; i++) {
-            for (int j=0; j<this.column; j++) {
-                result.matrix[i][j] = this.getElement(i, j) + other.getElement(i, j);
-            }
+        if(!this.isSameOrder(other)){
+            return null;
         }
-        return result;
+        else{
+            Matrix result = new Matrix(this.row, this.column);
+            for (int i = 0; i<this.row ; i++) {
+                for (int j=0; j<this.column; j++) {
+                    result.matrix[i][j] = this.getElement(i, j) + other.getElement(i, j);
+                }
+            }
+            return result;
+        }
+    }
+
+    public Matrix findProduct(Matrix other){
+        if(!(this.column == other.row))
+        return null;
+        else{
+            Matrix result = new Matrix(this.row, other.column);
+            for ( int i = 0 ; i < this.row ; i++ ) {
+                for ( int j = 0 ; j < other.column ; j++ ) {
+                    for ( int k = 0 ; k < other.row ; k++ ){
+                        sum = sum + this.getElement(i,k)*other.getElement(k,j);
+                    }
+                    result.matrix[i][j] = sum;
+                    sum = 0;
+                }
+            }
+            return result;
+        }
     }
 
 };
--- MatrixTest.java
+++ MatrixTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
 public class MatrixTest{
@@ -8,6 +9,7 @@ public class MatrixTest{
         Matrix matrix = new Matrix(1,1);
         matrix.populateMatrix(0,0,1);
         assertEquals(1,matrix.getElement(0,0));
+
     }
     @Test
     public void Matrix2By2(){
@@ -29,7 +31,7 @@ public class MatrixTest{
         Matrix matrix = new Matrix(1,1);
         matrix.populateMatrix(0,0,1);
 
-        Matrix secondMatrix = new Matrix(2,2);
+        Matrix secondMatrix = new Matrix(1,1);
         secondMatrix.populateMatrix(0,0,2);
 
         Matrix result = matrix.addMatrix(secondMatrix);
@@ -56,6 +58,101 @@ public class MatrixTest{
         assertEquals(5, result.getElement(0,1));
         assertEquals(7, result.getElement(1,0));
         assertEquals(9, result.getElement(1,1));
+    }
+
+    @Test
+    public void addUnEqualMatrix(){
+
+        Matrix matrix = new Matrix(1,1);
+        matrix.populateMatrix(0,0,1);
+
+        Matrix secondMatrix = new Matrix(2,1);
+        secondMatrix.populateMatrix(0,0,2);
+
+        assertNull(matrix.addMatrix(secondMatrix));
+    }
+
+    @Test
+    public void ProductOfMatrices(){
+
+        Matrix matrix = new Matrix(1,1);
+        matrix.populateMatrix(0,0,1);
+
+        Matrix secondMatrix = new Matrix(1,1);
+        secondMatrix.populateMatrix(0,0,2);
+
+        Matrix result = matrix.findProduct(secondMatrix);
+        assertEquals(2, result.getElement(0,0));
+    }
+
+    @Test
+    public void ProductOfMatrices2By2(){
+
+        Matrix matrix = new Matrix(2,2);
+        matrix.populateMatrix(0,0,1);
+        matrix.populateMatrix(0,1,2);
+        matrix.populateMatrix(1,0,3);
+        matrix.populateMatrix(1,1,4);
+
+        Matrix secondMatrix = new Matrix(2,2);
+        secondMatrix.populateMatrix(0,0,2);
+        secondMatrix.populateMatrix(0,1,0);
+        secondMatrix.populateMatrix(1,0,1);
+        secondMatrix.populateMatrix(1,1,2);
+
+        Matrix result = matrix.findProduct(secondMatrix);
+        assertEquals(4, result.getElement(0,0));
+        assertEquals(4, result.getElement(0,1));
+        assertEquals(10, result.getElement(1,0));
+        assertEquals(8, result.getElement(1,1));
+    }
+
+    @Test
+    public void ProductOfMatricesDifferentDimension(){
+
+        Matrix matrix = new Matrix(2,4);
+        matrix.populateMatrix(0,0,1);
+        matrix.populateMatrix(0,1,2);
+        matrix.populateMatrix(0,2,3);
+        matrix.populateMatrix(0,3,4);
+        matrix.populateMatrix(1,0,5);
+        matrix.populateMatrix(1,1,6);
+        matrix.populateMatrix(1,2,7);
+        matrix.populateMatrix(1,3,8);
+
+        Matrix secondMatrix = new Matrix(4,3);
+        secondMatrix.populateMatrix(0,0,1);
+        secondMatrix.populateMatrix(0,1,2);
+        secondMatrix.populateMatrix(0,2,3);
+        secondMatrix.populateMatrix(1,0,4);
+        secondMatrix.populateMatrix(1,1,5);
+        secondMatrix.populateMatrix(1,2,6);
+        secondMatrix.populateMatrix(2,0,7);
+        secondMatrix.populateMatrix(2,1,8);
+        secondMatrix.populateMatrix(2,2,9);
+        secondMatrix.populateMatrix(3,0,10);
+        secondMatrix.populateMatrix(3,1,11);
+        secondMatrix.populateMatrix(3,2,12);
+
+        Matrix result = matrix.findProduct(secondMatrix);
+        assertEquals(70, result.getElement(0,0));
+        assertEquals(80, result.getElement(0,1));
+        assertEquals(90, result.getElement(0,2));
+        assertEquals(158, result.getElement(1,0));
+        assertEquals(184, result.getElement(1,1));
+        assertEquals(210, result.getElement(1,2));
+
+    }
+    @Test
+    public void ProductOfMatricesInUnSatisfiedCondition(){
+
+        Matrix matrix = new Matrix(1,1);
+        matrix.populateMatrix(0,0,1);
+
+        Matrix secondMatrix = new Matrix(2,1);
+        secondMatrix.populateMatrix(0,0,2);
+
+        assertNull(matrix.findProduct(secondMatrix));
     }
 
 }
